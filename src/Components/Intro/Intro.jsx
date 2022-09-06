7ca9a7bc70cfd263cef8b4c9f4bb51161d33dd95
Piotr Gryko <pigryko@yahoo.com>
2022-09-06 23:11:37 +0100

    Intro basic

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro = () => {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hi! I am </span>
+                <span>Piotr Gryko </span>
+                <span>Frontend Developer with high level of experience in web designing and development, producing the Quality work </span>
+            </div>
+            <button className="button i-button">Hire me</button>
+        </div>
+        <div className="i-right">
+
+        </div>
+    </div>
+  )
+}
+
+export default Intro
