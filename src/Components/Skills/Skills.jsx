9ed88acb54c351db4dd55753d1c779372404b79d
Piotr Gryko <pigryko@yahoo.com>
2022-09-08 10:05:16 +0100

    Skills section done without blur

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from '../../img/heartemoji.png'
 import Glasses from '../../img/glasses.png'
 import Humble from '../../img/humble.png'
 import Card from '../Card/Card'
+import Resume from './Piotr Gryko_CV.pdf'
 
 const Skills = () => {
   return (
@@ -15,17 +16,34 @@ const Skills = () => {
         <span>
             This is my greate skill sets. I can do them all. I am soo good and 3d and web development. Hire me.
         </span>
-        <button className="button s-button">Download CV</button>
+        <a href={Resume} download>
+            <button className="button s-button">Download CV</button>
+        </a>
         <div classNAme="blur s-blur" style={{background: "#ABF1FF94"}}></div>
         </div>
         {/* right side  */}
         <div className="cards">
-            <div>
+            <div style={{left: '14rem' }}>
+
                 <Card 
                     emoji = {HeartEmoji}
                     heading = {'Design'}
                     detail = {"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
-                    />
+                />
+            </div>
+            <div style={{top:"12rem", left: "-4rem"}}>
+                <Card 
+                    emoji = {Glasses}
+                    heading = {'Developer'}
+                    detail = {"Html, Css, Javascript, React"}
+                />
+            </div>
+            <div style={{top:"19rem", left: "12rem"}}>
+                <Card 
+                    emoji = {Humble}
+                    heading = {'3D modelling'}
+                    detail = {"Blender 3D, Sketchup"}
+                />
             </div>
         </div>
     </div>
