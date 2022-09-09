dc4c56ee17a6895fe0664679362493d9ec05151b
Piotr Gryko <pigryko@yahoo.com>
2022-09-09 12:38:46 +0100

    Work icons added

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Works.css'
+import Upwork from "../../img/Upwork.png"
+import Fiverr from "../../img/fiverr.png"
+import Amazon from "../../img/amazon.png"
+import Shopify from "../../img/Shopify.png"
+import Facebook from "../../img/Facebook.png"
 
 const Works = () => {
   return (
@@ -16,6 +21,25 @@ const Works = () => {
             <button className="button s-button">Hire Me</button>
 
         <div className="blur s-blur1" style={{background: "var(--purple"}}></div>
+        </div>
+        <div className="w-right">
+            <div className="w-mainCircle">
+                <div className="w-secCircle">
+                    <img src={Upwork} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img src={Fiverr} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img src={Amazon} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img src={Shopify} alt="" />
+                </div>
+                <div className="w-secCircle">
+                    <img src={Facebook} alt="" />
+                </div>
+            </div>
         </div>
     </div>
   )
