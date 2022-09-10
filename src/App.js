67dd84d220b1bc59a74adc96258c6116feb5fc1f
Piotr Gryko <pigryko@yahoo.com>
2022-09-10 14:31:51 +0100

    Added Testimonial Section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Intro from './Components/Intro/Intro';
 import Navbar from './Components/Navbar/Navbar';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Skills from './Components/Skills/Skills';
+import Testimonial from './Components/Testimonials/Testimonial';
 import Works from './Components/Works/Works';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Testimonial />
     </div>
   );
 }
